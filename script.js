ce94c344f03f90b62ec32ed95f6ff307a104ef7f
GuilhermeCostaDF <guido.costa@globo.com>
2022-06-07 20:22:07 -0300

    Configurando paginacao

--- script.js
+++ script.js
@@ -2,7 +2,30 @@ $('document').ready(
 
     function(){
 
-        
+        $('a').on('click', function(e){
+            console.log($(this)[0].id)
+
+            switch($(this)[0].id){
+                case 'pgSobre':
+                    $('#divSobre')[0].style = "display: block;"
+                    $('#divQuemSou')[0].style = "display: none;"  
+                    // $('#divSudoku')[0].style = "display: none;"           
+                    break;
+                case 'pgQuemSou':
+                    $('#divQuemSou')[0].style = "display: block;"  
+                    $('#divSobre')[0].style = "display: none;"
+                    // $('#divSudoku')[0].style = "display: none;"
+                    break;
+                case 'pgJogar':
+                    // $('#divQuemSou')[0].style = "display: block;"  
+                    // $('#divSobre')[0].style = "display: none;"
+                    // $('#divSudoku')[0].style = "display: none;"
+                    break;
+                default:
+                    return;
+            }
+
+        })
 
 
     }
